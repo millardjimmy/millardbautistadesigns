650e8d5113385284e1daf27a02f67eff205f9e46
millardjimmy <millardjimmy@gmail.com>
2020-08-01 15:30:55 -0600

    task table build out

--- db/migrate/20200801211101_create_tasks.rb
+++ db/migrate/20200801211101_create_tasks.rb
@@ -1,6 +1,11 @@
 class CreateTasks < ActiveRecord::Migration[6.0]
   def change
     create_table :tasks do |t|
+      t.string :content
+      t.text :lead_notes
+      t.boolean :completed, default: false
+      t.integer :project_id
+      t.integer :employee_id
 
       t.timestamps
     end
