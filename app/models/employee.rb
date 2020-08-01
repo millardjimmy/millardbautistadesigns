68c8b2f04897929ccd7e11c824ee96af82f03cfc
millardjimmy <millardjimmy@gmail.com>
2020-08-01 15:43:58 -0600

    employee relationships

--- app/models/employee.rb
+++ app/models/employee.rb
@@ -1,2 +1,8 @@
 class Employee < ApplicationRecord
+    has_secure_password
+    belongs_to :company
+    has_many :tasks
+    has_many :projects, through: :tasks
+    has_many :comments
+    has_many :commented_projects, through: :comments, source: :projects
 end
