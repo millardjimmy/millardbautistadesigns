79123cfd55a952eee2a29dfc308b88590de859c2
millardjimmy <millardjimmy@gmail.com>
2020-08-01 15:52:56 -0600

    crud spine

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+
+    def logged_in?
+    end
+
+    def require_login
+    end
+
+    def current_user
+    end
+
+    def set_user
+    end
+
+    def project_lead?
+    end
+
+    def in_company?
+    end
+
+
 end
