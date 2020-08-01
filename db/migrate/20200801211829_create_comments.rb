8a602a2cf6ae380103473736c78bdd5e84f7264a
millardjimmy <millardjimmy@gmail.com>
2020-08-01 15:24:04 -0600

    comments table build out

--- db/migrate/20200801211829_create_comments.rb
+++ db/migrate/20200801211829_create_comments.rb
@@ -1,6 +1,9 @@
 class CreateComments < ActiveRecord::Migration[6.0]
   def change
     create_table :comments do |t|
+      t.text :content
+      t.integer :employee_id
+      t.integer :task_id
 
       t.timestamps
     end
