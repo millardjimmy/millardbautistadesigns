c89425a77d69df348e8cd8d08263963a509a5b8f
millardjimmy <millardjimmy@gmail.com>
2020-08-01 15:26:41 -0600

    employee table buildout

--- db/migrate/20200801211404_create_employees.rb
+++ db/migrate/20200801211404_create_employees.rb
@@ -1,6 +1,12 @@
 class CreateEmployees < ActiveRecord::Migration[6.0]
   def change
     create_table :employees do |t|
+      t.string :first_name
+      t.string :last_name
+      t.string :title
+      t.boolean :lead
+      t.string :email
+      t.string :password_digest
 
       t.timestamps
     end
