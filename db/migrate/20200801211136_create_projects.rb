b0767dda5e562592a6d2919c95eb181c757c22c2
millardjimmy <millardjimmy@gmail.com>
2020-08-01 15:31:06 -0600

    project table buildout

--- db/migrate/20200801211136_create_projects.rb
+++ db/migrate/20200801211136_create_projects.rb
@@ -1,6 +1,10 @@
 class CreateProjects < ActiveRecord::Migration[6.0]
   def change
     create_table :projects do |t|
+      t.string :name
+      t.datetime :due_date
+      t.integer :lead_id
+      t.text :description
 
       t.timestamps
     end
