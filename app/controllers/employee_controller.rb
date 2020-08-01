c53b91fdae2d2079c30535c82488e15fd7eb6a78
millardjimmy <millardjimmy@gmail.com>
2020-08-01 15:52:47 -0600

    crud spine

--- app/controllers/employee_controller.rb
+++ app/controllers/employee_controller.rb
@@ -1,2 +1,21 @@
 class EmployeeController < ApplicationController
+
+    def index
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+    
 end
