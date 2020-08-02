d62d3df7c94016b59e45630d44753f073c96b428
millardjimmy <millardjimmy@gmail.com>
2020-08-02 14:29:06 -0600

    roles for project leads exist

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -1,40 +1,71 @@
 class ProjectsController < ApplicationController
+    before_action :require_login
+    before_action :set_project, only: [:show, :edit, :update, :destroy]
+    before_action :set_user, only:[:index, :new, :create, :edit, :update, :show]
 
     def index
-        @projects = # current projects
+        @projects = Project.active_projects(session[:company_id])
     end
-
-    def new 
+    
+    def new
         @project = Project.new
     end
 
     def create
         @project = Project.new(project_params)
         if @project.save
-            redirect_to project_path
+            redirect_to project_path(@project)
         else
             render :new
+        end
     end
 
     def show
     end
 
     def edit
-        #lead will have ability to edit
+        if :project_lead?
+            render "edit"
+        else
+            return head(:forbidden)
+        end
     end
 
     def update
-        #lead will have ability to update
+        if :project_lead?
+            if @project.update(project_params)
+                redirect_to project_path(@project)
+            else
+                render :edit
+            end
+        else
+            return head(:forbidden)
+        end
     end
-    
+
     def destroy
-        #lead will have ability to delete
+        if :project_lead?
+            @project.tasks.each do |task|
+                task.comments.each do |comment|
+                    comment.destroy
+                end
+                task.destroy
+            end
+            @project.destroy
+            redirect_to projects_path
+        else
+            return head(:forbidden)
+        end
     end
 
     private
 
     def project_params
-        params.require(:project).permit(:name, :due_date, :description, :organization, :lead_id)
+        params.require(:project).permit(:name, :due_date, :description, :company_id, :lead_id)
     end
 
+    def set_project
+        @project = Project.find(params[:id])
+        require_company_permission(@project.company_id)
+    end
 end
