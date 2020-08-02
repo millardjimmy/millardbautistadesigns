dd992d9c3dca518899a7bc9b7cbbe39127b93e55
millardjimmy <millardjimmy@gmail.com>
2020-08-01 19:43:26 -0600

    comment controller buildout

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,2 +1,38 @@
 class CommentsController < ApplicationController
+    before_action :require_login
+
+    def index
+    end
+    
+    def new
+        @user = current_user
+    end
+
+    def create
+        comment = Comment.new(comment_params)
+        if comment.save
+            redirect_to task_path(comment.task)
+        else
+            redirect_to task_path(params[:task_id]) 
+        end
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def destroy
+    end
+
+    private
+
+    def comment_params
+        params.require(:comment).permit(:content, :task_id, :employee_id)
+    end
+
 end
