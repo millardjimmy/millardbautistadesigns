716a143d358c6369609cea6a56dbdaef94ef5b09
millardjimmy <millardjimmy@gmail.com>
2020-08-01 20:29:47 -0600

    routes started

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,23 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  
+  root 'static_pages#home'
+  get '/signup', to: 'static_pages#signup'
+  post '/signup', to: 'organizations#create'
+  get '/login', to: 'static_pages#login'
+  get '/auth/facebook/callback' => 'sessions#create_fb'
+  post '/login', to: 'sessions#create'
+  post '/sessions/create', to: 'sessions#create'
+  delete '/logout', to: 'sessions#destroy'
+  get '/employees/ofthemonth', to: 'employees#month'
+  
+  resources :organizations do
+    resources :employees, shallow: true
+  end
+
+  resources :projects, shallow: true do
+    resources :tasks do
+      resources :comments
+    end
+  end
+  
 end
