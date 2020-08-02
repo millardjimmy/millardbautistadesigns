4fd226a78d980f82fd10f263117dc2d5a88e587d
millardjimmy <millardjimmy@gmail.com>
2020-08-02 14:27:09 -0600

    variable change

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
   get '/signup', to: 'static_pages#signup'
-  post '/signup', to: 'organizations#create'
+  post '/signup', to: 'companys#create'
   get '/login', to: 'static_pages#login'
   get '/auth/facebook/callback' => 'sessions#create_fb'
   post '/login', to: 'sessions#create'
@@ -10,7 +10,7 @@ Rails.application.routes.draw do
   delete '/logout', to: 'sessions#destroy'
   get '/employees/ofthemonth', to: 'employees#month'
   
-  resources :organizations do
+  resources :companys do
     resources :employees, shallow: true
   end
 
