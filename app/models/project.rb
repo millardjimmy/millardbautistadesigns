72e27639810d0525fe57dc80601aad946e44f291
millardjimmy <millardjimmy@gmail.com>
2020-08-02 14:30:26 -0600

    helpermethod spine

--- app/models/project.rb
+++ app/models/project.rb
@@ -3,4 +3,21 @@ class Project < ApplicationRecord
     has_many :tasks
     has_many :employees, through: :tasks
     has_many :comments, through: :tasks
+    validates :name, presence: true
+
+    def unassigned_project_tasks
+    end
+
+    def assigned_project_tasks
+    end
+
+    def completed_project_tasks
+    end
+
+    def self.active_project_ids(company_id)
+    end
+
+    def self.active_projects(company_id)
+    end
+    
 end
